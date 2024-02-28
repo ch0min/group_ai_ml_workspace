f9b910ee22ce7db2f889d008ac26b535ed32c465
oTuff <oscartuff@protonmail.com>
2024-02-28 16:16:24 +0100

    Styling and minor changes

--- src/main/Oscar_housing.py
+++ src/main/Oscar_housing.py
@@ -7,6 +7,9 @@ import numpy as np
 from zlib import crc32
 from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
 
+# --------------------------------------------------------------
+# 1. Find and Download a Dataset
+# --------------------------------------------------------------
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
@@ -24,6 +27,9 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
 # fetch_housing_data()
 
 
+# --------------------------------------------------------------
+# 2. Data Exploration
+# --------------------------------------------------------------
 def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join("../../", housing_path, "housing.csv")
     return pd.read_csv(csv_path)
@@ -38,6 +44,9 @@ df.describe()
 df.hist(bins=50, figsize=(20, 15))
 
 
+# --------------------------------------------------------------
+# 3. Generating Test Set
+# --------------------------------------------------------------
 def split_train_test(data, test_ratio):
     shuffled_indices = np.random.permutation(len(data))
     test_set_size = int(len(data) * test_ratio)
@@ -64,25 +73,21 @@ def split_train_test_by_id(data, test_ratio, id_column):
 
 # use row index as unique identifier
 df_with_id = df.reset_index()
-
 train_set, test_set = split_train_test_by_id(df_with_id, 0.2, "index")
 
 
 # use row id based on longitude and latitude as unique identifier
 df_with_id["id"] = df["longitude"] * 1000 + df["latitude"]
-
 trains_set, test_set = split_train_test_by_id(df_with_id, 0.2, "id")
 
 # use sklearn to split df
 train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
-
-bins = [0, 2, 3, 5, 6, np.inf]  # Define your bins here
+bins = [0, 1.5, 3, 4.5, 6, np.inf]  # Define your bins here
 labels = [1, 2, 3, 4, 5]  # Labels for the bins
 
 # stratified sampling
 df["income_cat"] = pd.cut(df["median_income"], bins=bins, labels=labels)
-
 df["income_cat"].hist()
 
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
@@ -94,3 +99,10 @@ strat_test_set["income_cat"].value_counts() / len(strat_test_set)
 
 for set_ in (strat_train_set, strat_test_set):
     set_.drop("income_cat", axis=1, inplace=True)
+
+
+# --------------------------------------------------------------
+# 3. Data Visualization
+# --------------------------------------------------------------
+
+df = strat_train_set.copy()
